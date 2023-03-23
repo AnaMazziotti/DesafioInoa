341a878a378272b2eef93e4d5390a4466fe46998
Ana Lívia <analivia.200012@gmail.com>
2023-03-23 20:59:50 -0300

    tirei repetição do bloco de envio de email

--- StockQuoteAlert/Program.cs
+++ StockQuoteAlert/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System.Configuration;
 using System.Net;
 using System.Net.Mail;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
 
 
 namespace TesteAPI
@@ -14,9 +16,17 @@ namespace TesteAPI
             string ativo = args[0];
             decimal precoRefVenda, precoRefCompra;
 
+            //validações para os argumentos de entrada do programa
+
             if (args.Length != 3)
             {
                 Console.WriteLine("Número incorreto de parâmetros. Você deve seguir o padrão: StockQuoteAlert.exe [código do ativo] [preço de venda] [preço de compra]");
+                return;
+            }
+
+            if (args.Length == 0 || string.IsNullOrWhiteSpace(args[0]))
+            {
+                Console.WriteLine("O primeiro argumento não pode ser nulo ou vazio.");
                 return;
             }
 
@@ -33,22 +43,24 @@ namespace TesteAPI
                 return;
             }
 
-
+            //URL DA API
             string QUERY_URL = "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL";
             //Opções de moeda para colocar na linha de comando para essa API de teste: USD,EUR,BTC
             //Usei uma API de moedas porque era a melhor opção gratuita disponível em tempo real
+
 
             using (HttpClient client = new HttpClient())
             {
                 while (true) //monitoramento contínuo
                 {
+                    
                     //fazendo requisição para a API
                     HttpResponseMessage respostaAPI = await client.GetAsync(QUERY_URL);
                     string json_string = await respostaAPI.Content.ReadAsStringAsync();
                     Dictionary<string, JsonElement> json_data = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(json_string);
-
-
-
+                   
+                    
+                    //tratamento de exceções quando o programa tenta acessar uma chave inexistente no dicionário de resposta da API.
                     try
                     {
 
@@ -74,23 +86,20 @@ namespace TesteAPI
                         var senha = ConfigurationManager.AppSettings["senha"];
                         var host = ConfigurationManager.AppSettings["host"];
                         int port = Int32.Parse(ConfigurationManager.AppSettings["port"]);
+                        string body, subject, message;
 
-                        //inhas de código usadas para testes de disparo de email antes da integração com a API
-                        /*Console.Write("Digite um valor: ");
-                        string input = Console.ReadLine();
-                        int cotacao = int.Parse(input);*/
 
-                        string body, subject;
                         if (PrecoCompraFinal < precoRefCompra)
                         {
                             subject = "Alerta de compra teste INOA";
                             body = "Recomendamos que você compre o ativo " + ativo;
+                            message = "Email de compra enviado";
 
                             //ENVIAR O EMAIL COMPRA
-                            try
+                            /*try
                             {
 
-                                MailMessage mailMessage1 = new MailMessage(remetente, destinatario);
+                                MailMessage mailMessage1 = new MailMessage(remetente, destinatario); 
 
                                 mailMessage1.Subject = subject;
                                 mailMessage1.Body = body;
@@ -113,42 +122,48 @@ namespace TesteAPI
                             {
                                 Console.WriteLine("Houve um erro no envio do email de compra.");
                                 Console.WriteLine(ex.Message);
-                            }
+                            }*/
+
+                            EnviarEmail(remetente, destinatario, subject, body, port, host, senha, message);
 
                         }
                         else if (PrecoVendaFinal > precoRefVenda)
                         {
                             subject = "Alerta de venda teste INOA";
                             body = "Recomendamos que você venda o ativo " + ativo;
+                            message = "Email de venda enviado";
+
 
                             //ENVIAR O EMAIL VENDA
-                            try
-                            {
+                            /*   try
+                               {
 
-                                MailMessage mailMessage2 = new MailMessage(remetente, destinatario);
+                                   MailMessage mailMessage2 = new MailMessage(remetente, destinatario);
 
-                                mailMessage2.Subject = subject;
-                                mailMessage2.Body = body;
-                                mailMessage2.SubjectEncoding = Encoding.GetEncoding("UTF-8"); 
-                                mailMessage2.BodyEncoding = Encoding.GetEncoding("UTF-8"); 
+                                   mailMessage2.Subject = subject;
+                                   mailMessage2.Body = body;
+                                   mailMessage2.SubjectEncoding = Encoding.GetEncoding("UTF-8"); 
+                                   mailMessage2.BodyEncoding = Encoding.GetEncoding("UTF-8"); 
 
-                                SmtpClient smtpClient2 = new SmtpClient(host, port);
+                                   SmtpClient smtpClient2 = new SmtpClient(host, port);
 
-                                smtpClient2.UseDefaultCredentials = false;
-                                smtpClient2.Credentials = new NetworkCredential(remetente, senha);
+                                   smtpClient2.UseDefaultCredentials = false;
+                                   smtpClient2.Credentials = new NetworkCredential(remetente, senha);
 
-                                smtpClient2.EnableSsl = true;
+                                   smtpClient2.EnableSsl = true;
 
-                                smtpClient2.Send(mailMessage2);
+                                   smtpClient2.Send(mailMessage2);
 
-                                Console.WriteLine("Email de venda enviado.");
-                            }
+                                   Console.WriteLine("Email de venda enviado.");
+                               }
 
-                            catch (Exception ex)
-                            {
-                                Console.WriteLine("Houve um erro no envio do email de venda.");
-                                Console.WriteLine(ex.Message);
-                            }
+                               catch (Exception ex)
+                               {
+                                   Console.WriteLine("Houve um erro no envio do email de venda.");
+                                   Console.WriteLine(ex.Message);
+                               }*/
+
+                            EnviarEmail(remetente, destinatario, subject, body, port, host, senha, message);
                         }
                         else
                         {
@@ -164,6 +179,35 @@ namespace TesteAPI
                     }
 
                     await Task.Delay(30000); //monitoramento contínuo de 30 em 30 segundos
+                }
+            }
+
+            void EnviarEmail(string remetente, string destinatario, string subject, string body, int port, string host, string senha, string message)
+            {
+                try
+                {
+                    MailMessage mailMessage = new MailMessage(remetente, destinatario);
+
+                    mailMessage.Subject = subject;
+                    mailMessage.Body = body;
+                    mailMessage.SubjectEncoding = Encoding.GetEncoding("UTF-8");
+                    mailMessage.BodyEncoding = Encoding.GetEncoding("UTF-8");
+
+                    SmtpClient smtpClient = new SmtpClient(host, port);
+
+                    smtpClient.UseDefaultCredentials = false;
+                    smtpClient.Credentials = new NetworkCredential(remetente, senha);
+
+                    smtpClient.EnableSsl = true;
+
+                    smtpClient.Send(mailMessage);
+
+                    Console.WriteLine(message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Houve um erro no envio do email.");
+                    Console.WriteLine(ex.Message);
                 }
             }
         }
